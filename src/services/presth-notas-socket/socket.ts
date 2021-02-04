fca28f878e0c05be4da1716c03956fb6f6942d29
Anurag Gupta <anuragg569@gmail.com>
2021-02-04 20:35:57 +0530

    Replaced wss with ws in socket.ts

--- src/services/presth-notas-socket/socket.ts
+++ src/services/presth-notas-socket/socket.ts
@@ -11,7 +11,7 @@ import { GLOBAL_APP_STATUS } from '../../global';
 class PresthNotasSocket {
     private static instance: PresthNotasSocket;
 
-    private connString: string = 'ws://pickiser.com:4000';
+    private connString: string = 'wss://pickiser.com:4000';
     private connection: WebSocket = {} as any;
     private status = {
         isConnected: false
